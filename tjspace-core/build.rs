b9b5b7e8aca0bcc6e56ab9aa4b7bd52ed09b687d
beyondhorizen05-sys <beyondhorizen05@gmail.com>
2026-09-22 01:34:54 +0400

    build(system-12): generate gRPC sync bindings without protoc

--- tjspace-core/build.rs
+++ tjspace-core/build.rs
@@ -1,1 +1,9 @@
-fn main(){println!("cargo:rerun-if-changed=migrations");}
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let fds = protox::compile(["proto/state_sync.proto"], ["proto"])?;
+    tonic_prost_build::configure()
+        .build_client(true)
+        .build_server(true)
+        .compile_fds(fds)?;
+    println!("cargo:rerun-if-changed=proto/state_sync.proto");
+    Ok(())
+}
